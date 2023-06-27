3838ab7be3d80ee7c522ce152683aa5e07a69774
Dachev <dachev1919@gmail.com>
2023-06-27 12:30:57 +0300

    added footer

--- src/common/components/footer/Footer.jsx
+++ src/common/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import styles from './Footer.module.scss';
+import { BiUpArrowAlt } from 'react-icons/bi';
 
 const Footer = () => {
 	return (
-		<div>
-			
-		</div>
+		<footer className={styles.footer}>
+			<div className={styles.text}>
+				<p>Copyright &copy; 2023 by Dachev | All Rights Reserved.</p>
+			</div>
+			<div className={styles.icon}>
+				<a rel='noreferrer' href='#hero'>
+					<BiUpArrowAlt />
+				</a>
+			</div>
+		</footer>
 	);
 };
 
